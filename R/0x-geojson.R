d6f97687909154b90ab0b9ef79c4020df797e383
ElHabr, Tony <anthonyelhabr@gmail.com>
2018-07-19 09:30:49 -0500

    updated readme and bits of each Rmd, removed shapefile stuff with gitignore

--- R/0x-geojson.R
+++ R/0x-geojson.R
@@ -1,20 +1,14 @@
 
-json0 <-
-  httr::GET("https://opendata.arcgis.com/datasets/059432fd0dcb4a208974c235e837c94f_0.geojson")
-json1$features
-json1 <-
-  jsonlite::fromJSON("https://opendata.arcgis.com/datasets/059432fd0dcb4a208974c235e837c94f_0.geojson")
-json1$features
-json1$features$properties
-json1$features$geometry
-
-df <-
-  bind_cols(
-    json1$features$properties,
-    json1$features$geometry
-  )
-pnts <-
-  json1$features$geometry[,2] %>% sp::SpatialPoints()
-pnts
-json2 <-
-  geojson::read("https://opendata.arcgis.com/datasets/059432fd0dcb4a208974c235e837c94f_0.geojson")
+# Reference: https://github.com/Robinlovelace/Creating-maps-in-R/blob/master/vignettes/geoJSON.Rmd.
+library("tidyverse")
+url <- "https://opendata.arcgis.com/datasets/059432fd0dcb4a208974c235e837c94f_0.geojson"
+path_dest <- file.path("data-raw", "geojson")
+downloader::download(url = url, destfile = path_dest)
+data_raw <-
+  rgdal::readOGR(dsn = path_dest)
+data_raw <-
+  path_dest %>%
+  sf::st_read() %>%
+  as_tibble() %>%
+  mutate_if(is.factor, funs(as.character)) %>%
+  janitor::clean_names()
--- README.md
+++ README.md
@@ -7,13 +7,33 @@ See my blog posts (to be linked here) for the full write-ups, or, check out the 
 The documents can be recreated with the following commands:
 
 ``` r
-rmarkdown::render("R/01-report.Rmd", output_dir = "output", intermediates_dir = "output")
+# rmarkdown::render("R/01-tea_uil_cors.Rmd", output_dir = "output", intermediates_dir = "output")
+paths <-
+  list.files(
+    path = "R",
+    pattern = "Rmd$",
+    recursive = FALSE,
+    full.names = TRUE
+  )
+dir_output <- "output"
+purrr::map(
+  paths[1],
+  ~rmarkdown::render(
+    .x,
+    output_dir = dir_output, 
+    intermediates_dir = dir_output
+  )
+)
 ```
 
 Highlights
 ==========
 
 Here are a couple of the coolest visualizations, in my opinion.
+
+![](figs/viz_schools_grid.png)
+
+![](figs/viz_schools_chlr.png)
 
 ![](figs/viz_schools_tea_cors_byyear.png)
 
